923a09743250b931f046c0bcaf323d58bab971fa
Vitaly <vitaly_subbotin@mail.ru>
2020-06-28 21:13:15 +0300

    initial commit

---
+++ ImageLocker/ImageLocker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ImageLocker
+//
+//  Created by Виталий Субботин on 28.06.2020.
+//  Copyright © 2020 Виталий Субботин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
