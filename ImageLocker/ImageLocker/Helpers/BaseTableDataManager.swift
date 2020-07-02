49adf5bd662af19c2437975bee74617764fbb371
Субботин Виталий Александрович <subbotin.v.aleksa@sberbank.ru>
2020-07-03 00:33:11 +0300

    add BaseTableDataManager

--- ImageLocker/ImageLocker/Helpers/BaseTableDataManager.swift
+++ ImageLocker/ImageLocker/Helpers/BaseTableDataManager.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
-class BaseTableDataManager {
+protocol ConfigurableCell where Self: UITableViewCell {
+    associatedtype CellModel
+    func configure(model: CellModel)
+}
+
+protocol CellConfigurator {
+    static var reuseId: String { get }
+    var height: CGFloat { get set }
+    func configure(cell: UITableViewCell)
+}
+
+class TableCellConfigurator<CellType: ConfigurableCell, CellModel>: CellConfigurator where CellType.CellModel == CellModel {
     
+    static var reuseId: String { return String(describing: CellType.self) }
+    var model: CellModel
+    var height: CGFloat
+    
+    init(model: CellModel, height: CGFloat) {
+        self.model = model
+        self.height = height
+    }
+    
+    func configure(cell: UITableViewCell) {
+        (cell as? CellType)?.configure(model: model)
+    }
+}
+
+class BaseTableDataManager: NSObject, UITableViewDataSource {
+    var items: [CellConfigurator] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: type(of: item).reuseId, for: indexPath)
+        item.configure(cell: cell)
+        
+        return cell
+    }
 }
