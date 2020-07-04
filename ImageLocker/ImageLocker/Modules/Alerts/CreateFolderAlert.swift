2f8e4679fe9506d077fa4b1092af80d68d9760e8
Субботин Виталий Александрович <subbotin.v.aleksa@sberbank.ru>
2020-07-04 13:29:09 +0300

    add CreateFolderAlert

---
+++ ImageLocker/ImageLocker/Modules/Alerts/CreateFolderAlert.swift
@@ -0,0 +1,51 @@
+//
+//  CreateFolderAlert.swift
+//  ImageLocker
+//
+//  Created by Виталий Субботин on 03.07.2020.
+//  Copyright © 2020 Виталий Субботин. All rights reserved.
+//
+
+import UIKit
+
+class CreateFolderAlert: UIViewController {
+    var alertTitle = ""
+    var alertMessage = ""
+    var resultHandler: ((String) -> Void)?
+    
+    private enum LocalizedString {
+        static let placeholder = "Введите название папки"
+        static let create = "Создать"
+        static let cancel = "Отмена"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showAlert()
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = LocalizedString.placeholder
+        }
+        let create = UIAlertAction(title: LocalizedString.create, style: .default) { [weak self] _ in
+            guard let textField = alert.textFields?.first, let text = textField.text else {
+                self?.resultHandler?("")
+                return
+            }
+            self?.resultHandler?(text)
+        }
+        let cancel = UIAlertAction(title: LocalizedString.cancel, style: .cancel) { [weak self] _ in
+            self?.dismiss(animated: false)
+        }
+        alert.addAction(create)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+}
--- ImageLocker/ImageLocker/Modules/Folders/FoldersViewController.swift
+++ ImageLocker/ImageLocker/Modules/Folders/FoldersViewController.swift
@@ -18,11 +18,20 @@ class FoldersViewController: UIViewController, FoldersViewInput {
     var presenter: FoldersViewOutput?
     var viewController: UIViewController { return self }
     
+    private enum LocalizedString {
+        static let title = "Папки"
+    }
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.separatorStyle = .none
         tableView.backgroundColor = .white
         return tableView
+    }()
+    
+    private lazy var addFolderButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFolderClicked(_:)))
+        return button
     }()
     
     override func loadView() {
@@ -33,9 +42,16 @@ class FoldersViewController: UIViewController, FoldersViewInput {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = LocalizedString.title
         navigationItem.hidesBackButton = true
+        navigationItem.rightBarButtonItem = addFolderButton
         makeConstraints()
         presenter?.viewDidLoad(self)
+    }
+    
+    
+    @objc private func addFolderClicked(_ sender: UIBarButtonItem) {
+        presenter?.viewDidShowCreateFolder(self)
     }
     
     private func makeConstraints() {
