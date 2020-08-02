874aa77dd5f2a4c9f557b4f16abc5da12de219da
Субботин Виталий Александрович <vitaly_subbotin@mail.ru>
2020-08-02 09:35:15 +0300

    PhotoPreview added

---
+++ ImageLocker/ImageLocker/Modules/PhotoPreview/PhotoPreviewViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoPreviewViewController.swift
+//  ImageLocker
+//
+//  Created by Виталий Субботин on 02.08.2020.
+//  Copyright © 2020 Виталий Субботин. All rights reserved.
+//
+
+import Foundation
